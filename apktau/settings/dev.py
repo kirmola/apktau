35fbfdb975ddc8b068b37a1e028e7596b3276017
Aman Rawat <ar.rawataman@gmail.com>
2024-03-15 21:19:22 +0530

    Added: Django debug toolbar

--- apktau/settings/dev.py
+++ apktau/settings/dev.py
@@ -17,5 +17,16 @@ DATABASES = {
 
 
 INSTALLED_APPS += [
+    'debug_toolbar'
+]
 
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
+]
+
+
+INTERNAL_IPS = [
+    '127.0.0.1'
 ]
--- apktau/urls_dev.py
+++ apktau/urls_dev.py
@@ -1,5 +1,8 @@
 from .urls import *
+from django.urls import include
 
 urlpatterns += [
-    
+    path("__debug__/", include("debug_toolbar.urls")),
+   
 ]
+
